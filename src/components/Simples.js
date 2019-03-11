a0f0b0bb9cbbfd1f2dabd30058d58a4f342baf19
Jesse Abreu <jesse.abreu@gdfnet.df.gov.br>
2019-03-11 12:28:09 -0300

    S3 - Primeiro Componente

---
+++ src/components/Simples.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default props => <Text>{props.texto}</Text>
+
+//Dica: Usar array (não esquecer Key) para retornar mais de um componente, ou retornar uma View com os componentes dentro!!!
