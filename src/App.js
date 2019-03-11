d285a6b9d9848cd10aa21197430803045964b00d
Jesse Abreu <jesse.abreu@gdfnet.df.gov.br>
2019-03-11 14:18:28 -0300

    S3 - Sistema de Modulo: Import/Export

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Paridade from './components/Paridade'
+import { Inverter, MegaSena } from './components/Multi'
 
 export default class App extends Component {
   render(){
     return (
       <View style={styles.container}>
         <Paridade numero={32} />
+        <Inverter texto='React Native!'></Inverter>
+        <MegaSena numeros={6}></MegaSena>
       </View>
     )
   }
