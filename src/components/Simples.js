388573c211168fa5b65379e770bfc629892d9b85
Jesse Abreu <jesse.abreu@gdfnet.df.gov.br>
2019-03-11 12:35:11 -0300

    S3 - Criando Estilo

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Default from '../style/Default'
 
-export default props => <Text>{props.texto}</Text>
+export default props => <Text style={Default.ex}>{props.texto}</Text>
 
 //Dica: Usar array (não esquecer Key) para retornar mais de um componente, ou retornar uma View com os componentes dentro!!!
